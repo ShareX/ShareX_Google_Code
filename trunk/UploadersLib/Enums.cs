7f5ef31d95f7d66672d4ec24f8f1f4c5253eb1f9
flexy123 <flexy123@46c6a946-90af-4660-e6c0-fd55b8419866>
2012-05-25 20:36:30 +0000

    fixed issue 25 - added pastee.org text uploader

--- trunk/UploadersLib/Enums.cs
+++ trunk/UploadersLib/Enums.cs
@@ -63,6 +63,8 @@ namespace UploadersLib
         Paste2,
         [Description("slexy.org")]
         Slexy,
+        [Description("pastee.org")]
+        Pastee,
         [Description("File Uploader")]
         FileUploader
     }
