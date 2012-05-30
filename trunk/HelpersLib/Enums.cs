ba0168fad2c3083c06a028fa61d295583832a512
mcored <mcored@46c6a946-90af-4660-e6c0-fd55b8419866>
2012-05-30 05:19:21 +0000

    added EnumToStringUsingDescription

--- trunk/HelpersLib/Enums.cs
+++ trunk/HelpersLib/Enums.cs
@@ -23,6 +23,7 @@
 
 #endregion License Information (GPL v3)
 
+using System;
 using System.ComponentModel;
 
 namespace HelpersLib
@@ -203,5 +204,41 @@ namespace HelpersLib
         FreeHandRegion,
         [Description("Last Region")]
         LastRegion
+    }
+
+    public class EnumToStringUsingDescription : TypeConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return (sourceType.Equals(typeof(Enum)));
+        }
+
+        public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+        {
+            return (destinationType.Equals(typeof(String)));
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+        {
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+        {
+            if (!destinationType.Equals(typeof(String)))
+            {
+                throw new ArgumentException("Can only convert to string.", "destinationType");
+            }
+
+            if (!value.GetType().BaseType.Equals(typeof(Enum)))
+            {
+                throw new ArgumentException("Can only convert an instance of enum.", "value");
+            }
+
+            string name = value.ToString();
+            object[] attrs =
+                value.GetType().GetField(name).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return (attrs.Length > 0) ? ((DescriptionAttribute)attrs[0]).Description : name;
+        }
     }
 }
