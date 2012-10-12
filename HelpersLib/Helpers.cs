335a830886193171953215dd7deb6c3df97e2439
Jaex <Jaex@users.noreply.github.com>
2012-10-12 07:29:13 +0000

    fixed issue 232 changed url validation method

--- HelpersLib/Helpers.cs
+++ HelpersLib/Helpers.cs
@@ -421,10 +421,53 @@ namespace HelpersLib
             if (!string.IsNullOrEmpty(url))
             {
                 url = url.Trim();
-                return !url.StartsWith("file://") && Uri.IsWellFormedUriString(url, UriKind.Absolute);
+                return !url.StartsWith("file://") && Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute);
             }
 
             return false;
+        }
+
+        public static bool IsValidURLRegex(string url)
+        {
+            // https://gist.github.com/729294
+
+            string pattern =
+                "^" +
+                // protocol identifier
+                "(?:(?:https?|ftp)://)" +
+                // user:pass authentication
+                "(?:\\S+(?::\\S*)?@)?" +
+                "(?:" +
+                // IP address exclusion
+                // private & local networks
+                "(?!10(?:\\.\\d{1,3}){3})" +
+                "(?!127(?:\\.\\d{1,3}){3})" +
+                "(?!169\\.254(?:\\.\\d{1,3}){2})" +
+                "(?!192\\.168(?:\\.\\d{1,3}){2})" +
+                "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+                // IP address dotted notation octets
+                // excludes loopback network 0.0.0.0
+                // excludes reserved space >= 224.0.0.0
+                // excludes network & broacast addresses
+                // (first & last IP address of each class)
+                "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+                "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+                "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+                "|" +
+                // host name
+                "(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)" +
+                // domain name
+                "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*" +
+                // TLD identifier
+                "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" +
+                ")" +
+                // port number
+                "(?::\\d{2,5})?" +
+                // resource path
+                "(?:/[^\\s]*)?" +
+                "$";
+
+            return Regex.IsMatch(url.Trim(), pattern, RegexOptions.IgnoreCase);
         }
 
         public static string GetUniqueFilePath(string folder, string filename)
