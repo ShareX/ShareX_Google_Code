bde9c07d0398596c8affeb5d4970764e0eb003b0
flexy123 <flexy123@46c6a946-90af-4660-e6c0-fd55b8419866>
2013-02-01 07:14:40 +0000

    fixed issue 297 region capture dpi problem

--- trunk/ScreenCapture.Shapes/Surface.cs
+++ trunk/ScreenCapture.Shapes/Surface.cs
@@ -100,6 +100,8 @@ namespace ScreenCapture
         private void InitializeComponent()
         {
             this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Bounds = ScreenRectangle;
             this.FormBorderStyle = FormBorderStyle.None;
             this.StartPosition = FormStartPosition.Manual;
