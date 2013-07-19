6cf0ff7a8a6cfc3c1036923d81aeca7a034926aa
flexy123 <flexy123@46c6a946-90af-4660-e6c0-fd55b8419866>
2013-07-19 14:27:15 +0000

    ShareX 7.2.0.436 released

--- trunk/ShareX/Properties/AssemblyInfo.cs
+++ trunk/ShareX/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("82E6AC09-0FEF-4390-AD9F-0DD3F5561EFC")]
-[assembly: AssemblyVersion("7.2.0.430")]
-[assembly: AssemblyFileVersion("7.2.0.430")]
+[assembly: AssemblyVersion("7.2.0.436")]
+[assembly: AssemblyFileVersion("7.2.0.436")]
