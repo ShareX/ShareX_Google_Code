6a7a1c8fcc455ca8356cfb3a9124d0ee5e654be7
McoreD <mcored@gmail.com>
2013-09-15 20:26:46 +0000

    Indexer Default.css is now stored in PersonalPath\Indexer\Default.css; if the file does not exist, it will be created.

--- ShareX/UploadTask.cs
+++ ShareX/UploadTask.cs
@@ -343,6 +343,10 @@ namespace ShareX
             {
                 if (Directory.Exists(tempText))
                 {
+                    if (string.IsNullOrEmpty(Info.TaskSettings.IndexerSettings.WorkingDir))
+                    {
+                        Info.TaskSettings.IndexerSettings.WorkingDir = Program.PersonalPath;
+                    }
                     tempText = Indexer.Run(tempText, Info.TaskSettings.IndexerSettings);
                 }
                 byte[] byteArray = Encoding.UTF8.GetBytes(tempText);
